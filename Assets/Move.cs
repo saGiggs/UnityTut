eb9d0af97931ab92bd1cee55b87c765f793faa92
sagiggs <sagiggs@gmail.com>
2017-06-09 10:35:59 -0400

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public Vector3 direction = Vector3.forward;
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		//transform.position += Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.rotation*(direction.normalized*speed*Time.deltaTime);
+	}
+}
---
+++ Assets/MoveLeftandRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftandRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Moves object left/right, up/down
+		transform.position += Vector3.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+		transform.position += Vector3.up * Input.GetAxis ("Vertical")*speed*Time.deltaTime;
+	}
+}
